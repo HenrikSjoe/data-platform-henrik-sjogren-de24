cbc33dea45793120c828364e7672139dbf98e064
Henrik <sjogren.henrik@gmail.com>
2025-01-09 11:59:21 +0100

    added python_repetion and exercises

---
+++ code_alongs/02_python_repetition/clean_text.py
@@ -0,0 +1,15 @@
+import re
+from pathlib import Path
+
+print("This is path of this script")
+print(Path(__file__).parent / "data" / "ml_text_raw.txt")
+
+data_path = Path(__file__).parent / "data"
+
+with open(data_path/"ml_text_raw.txt", 'r') as file:
+    raw_text = file.read()
+
+
+text_fixed_spacing = re.sub(r"\s+", " ", raw_text)
+
+print(text_fixed_spacing)
